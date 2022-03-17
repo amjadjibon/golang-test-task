5d89607c29250e286afce40665b2426ab002ae2f
Amjad Jibon <amjad.jibon@gmail.com>
2022-03-17 15:30:48 +0600

    testValidity added with unit test

---
+++ task1/testValidity.go
@@ -0,0 +1,5 @@
+package task1
+
+func testValidity(input string) bool {
+	return false
+}
---
+++ task1/testValidity_test.go
@@ -0,0 +1,32 @@
+package task1
+
+import (
+	"testing"
+)
+
+func TestTestValidity(t *testing.T) {
+	type input struct {
+		s string
+	}
+
+	type output struct {
+		valid bool
+	}
+
+	tests := []struct {
+		input input
+		want  output
+	}{
+		{input: input{s: "23-ab-48-caba-56-haha"}, want: output{valid: true}},
+		{input: input{s: "1-hello-2-world"}, want: output{valid: true}},
+		{input: input{s: ""}, want: output{valid: false}},
+		{input: input{s: "hello world"}, want: output{valid: false}},
+	}
+
+	for _, test := range tests {
+		got := testValidity(test.input.s)
+		if got != test.want.valid {
+			t.Errorf("testValidity(%q) == %t, want %t", test.input.s, got, test.want.valid)
+		}
+	}
+}
